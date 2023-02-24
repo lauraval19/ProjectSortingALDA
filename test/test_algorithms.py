3bfae05820d31c2eca3440b42244ac41db155599
unknown <laura.alvarado-g@labinfo.is.escuelaing.edu.co>
2023-02-24 08:43:27 -0500

    unit test finished

---
+++ test/test_algorithms.py
@@ -0,0 +1,28 @@
+import unittest
+import sortalgorithms.algorithms as algorithms
+
+arrays = [[1,9,8,2,3,4,5,7,6],[1],[2],[-11,10,25,100,-500,300,-250,1000],[10,8]]
+expected = [[1,2,3,4,5,6,7,8,9],[1],[2],[-500,-250,-11,10,25,100,300,1000],[8,10]]
+
+class AlgorithmsTest(unittest.TestCase):
+    def test_selection_sort(self):
+        for i in range(len(arrays)):
+            array = arrays[i]
+            result = algorithms.selection_sort(array)
+            self.assertTrue(result,expected[i])
+
+
+    def test_merge_sort(self):
+       for i in range(len(arrays)):
+            array = arrays[i]
+            result = algorithms.merge_sort(array)
+            self.assertTrue(result,expected[i])
+    
+    def test_insertion_sort(self):
+       for i in range(len(arrays)):
+            array = arrays[i]
+            result = algorithms.insertion_sort(array)
+            self.assertTrue(result,expected[i])
+    
+if __name__ == "__main__":
+    unittest.main()
